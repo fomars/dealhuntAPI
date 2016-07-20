27e7322197bfae2a313de470dcb928047ec97577
arseny <fomars@gmail.com>
2016-07-20 17:43:05 +0300

    settings template, requirements

--- dealhuntAPI/local_settings_template.py
+++ dealhuntAPI/local_settings_template.py
@@ -1,19 +1,29 @@
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+import os
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'db.sqlite3'),
+    },
+    'dealhunt': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'name',
-        'USER': 'username',
-        'PASSWORD': 'password',
-        'HOST': 'hostname',
+        'NAME': 'huy',
+        'USER': 'huy',
+        'PASSWORD': 'huy',
+        'HOST': 'huy',
         'PORT': '3306',
     }
 }
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1234567890'
+SECRET_KEY = 'huy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+BROKER_URL = 'redis://localhost:6379'
+
+REDIS_HOST = 'localhost'
+
+REDIS_PORT = 6379
